82dbb428f6384c597e7270671275bc166a981c9e
Burkay <burkayatar5@gmail.com>
2021-12-07 20:08:02 +0300

    Initial Commit

---
+++ StajProjesi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StajProjesi
+//
+//  Created by Burkay Atar on 7.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
